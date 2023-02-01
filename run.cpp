4698fbfee3de8a12ba1884fbc08cdf8b748487a6
grandnook <sb22893a@st.omu.ac.jp>
2023-02-01 23:22:01 +0900

    Add static values

--- run.cpp
+++ run.cpp
@@ -3,28 +3,44 @@
 #include <math.h>
 #include <queue>
 
-#define DEBUG 1
+#define DEBUG 0
 
 using namespace std;
 
 #define START_TIME 0
-#define FINISH_TIME 10
+#define FINISH_TIME 10000
+#define K 50
 
 double lambd = 1.0;
+double mu = 1.0;
 int global_pkt_id = 0;
-queue<int> pkt_queue;
+
+typedef struct _wait_Packet{
+    double w_arrival_time;
+    int w_packet_id;
+} wait_Packet;
+
+queue<wait_Packet> pkt_queue;
+
+int arrival_cnt = 0;    // Packet Arrival Count
+int departure_cnt = 0;  // Packet Departure Count
+int loss_cnt = 0;       // Packet Loss Count
+double interval_x_queue_sum = 0.0;
+double stay_time_sum = 0.0;
 
 // Uniform Random Number [0, 1) (一様乱数)
 double uni_rand(){
     random_device rand_dev;
     mt19937 mt32(rand_dev());
     uniform_real_distribution<double> uni_distribution(0, 1);
-    return uni_distribution(mt32);
+    double uni_rnd = uni_distribution(mt32);
+    return uni_rnd;
 }
 
 // Exponential Random Number (指数乱数)
-double exp_rand(){
-    return -log(1.0 - uni_rand()) / lambd;
+double exp_rand(double x){
+    double exp_rnd = -log(1.0 - uni_rand()) / x;
+    return exp_rnd;
 }
 
 enum class EVENT_TYPE{
@@ -70,33 +86,45 @@ int main(){
         EVENT_TYPE head_e_type = event_table.top().event_type;
         if (head_e_type == EVENT_TYPE::TERMINATOR){
 #if DEBUG
-            cout << "  event_type = Terminator" << endl;
+        cout << "  event_type = Terminator" << endl;
+        while(!event_table.empty()){
+            cout << "    arrival_time = " << event_table.top().arrival_time << endl;
+            cout << "    event_type = " << static_cast<int>(event_table.top().event_type) << endl;
+            cout << "    packet_id = " << event_table.top().packet_id << endl;
+            cout << "    pkt_queue = " << pkt_queue.size() << endl << endl;
+            delete_head_event();
+        }
 #endif
-            return 0;
+            break;
 
         }
         else if (head_e_type == EVENT_TYPE::ARRIVAL){
 #if DEBUG
             cout << "  event_type = Arrival" << endl;
 #endif
-            if (pkt_queue.size() == 0){
-                register_event(sim_time + exp_rand(), EVENT_TYPE::DEPARTURE, event_table.top().packet_id);
+            register_event(sim_time + exp_rand(lambd), EVENT_TYPE::ARRIVAL, global_pkt_id);
+
+            if (pkt_queue.size() < K){
+                if (pkt_queue.size() == 0){
+                    register_event(sim_time + exp_rand(mu), EVENT_TYPE::DEPARTURE, event_table.top().packet_id);
+                }
+                pkt_queue.push({sim_time, global_pkt_id++});
+            }else{
+                loss_cnt++;
             }
-
-            register_event(sim_time + exp_rand(), EVENT_TYPE::ARRIVAL, global_pkt_id);
-
-            pkt_queue.push(global_pkt_id++);
-
+            arrival_cnt++;
         }
         else if (head_e_type == EVENT_TYPE::DEPARTURE){
 #if DEBUG
             cout << "  event_type = Departure" << endl;
 #endif
             if (pkt_queue.size() > 1){
-                register_event(sim_time + exp_rand(), EVENT_TYPE::DEPARTURE, pkt_queue.front());
-            }            
-
+                register_event(sim_time + exp_rand(mu), EVENT_TYPE::DEPARTURE, pkt_queue.front().w_packet_id);
+            }
+            
+            stay_time_sum += sim_time - pkt_queue.front().w_arrival_time;
             pkt_queue.pop();
+            departure_cnt++;
 
         }
 #if DEBUG
@@ -106,10 +134,16 @@ int main(){
 #endif
 
         delete_head_event();
-        sim_time = event_table.top().arrival_time;
+        interval_x_queue_sum += (event_table.top().arrival_time - sim_time) * pkt_queue.size();
 
+        sim_time = event_table.top().arrival_time;
 #if DEBUG
         cout << "\n[sim_time] = " << sim_time << endl;
 #endif
     }
+
+    cout << "Average Packet Count = " << interval_x_queue_sum/(FINISH_TIME-START_TIME) << endl;
+    cout << "Average Stay Time = " << stay_time_sum/departure_cnt << endl;
+    cout << "Loss Rate = " << (double)loss_cnt/arrival_cnt << endl;
+    return 0;
 }
